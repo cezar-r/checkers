6625d9143de0d808ae829956f37141ece1e4f7e7
Cezar R <cez.rata1@gmail.com>
2022-03-02 16:15:53 -0700

    checkers ai

---
+++ src/checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+CROWN = pygame.transform.scale(pygame.image.load("assets/crown.png"), (44, 25))
+
---
+++ src/minimax/test.py
@@ -0,0 +1,7 @@
+import time
+
+start = time.process_time()
+
+for i in range(658000):
+	pass
+print(time.process_time() - start)
